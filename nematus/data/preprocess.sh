fee3c395033df69c234b3e2b5051a6031bf8f6ac
js <js@localhost.localdomain>
2020-07-30 13:39:01 -0400

    Added theano branch of nematus from https://github.com/EdinburghNLP/nematus/tree/theano

---
+++ nematus/data/preprocess.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# simple preprocessing script
+# a more sophisticated example, that includes truecasing and re-using the BPE segmentation for a test set,
+# can be found here: https://github.com/rsennrich/wmt16-scripts/tree/master/sample
+
+# stem of the file names of the parallel corpus (assumes that the file names are $P.$S (for the source) and $P.$T (for the target))
+P=$1
+
+# source language (example: fr)
+S=$2
+# target language (example: en)
+T=$3
+
+# path to nematus/data
+P1=$4
+
+# path to subword NMT scripts (can be downloaded from https://github.com/rsennrich/subword-nmt)
+P2=$5
+
+# tokenize
+perl $P1/tokenizer.perl -threads 5 -l $S < {P}.${S} > {P}.${S}.tok
+perl $P1/tokenizer.perl -threads 5 -l $T < {P}.${T} > {P}.${T}.tok
+
+# learn BPE on joint vocabulary:
+cat {P}.${S}.tok {P}.${T}.tok | python $P2/learn_bpe.py -s 20000 > ${S}${T}.bpe
+
+python $P2/apply_bpe.py -c ${S}${T}.bpe < {P}.${S}.tok > {P}.${S}.tok.bpe
+python $P2/apply_bpe.py -c ${S}${T}.bpe < {P}.${T}.tok > {P}.${T}.tok.bpe
+
+# build dictionary
+python $P1/build_dictionary.py {P}.${S}.tok.bpe
+python $P1/build_dictionary.py {P}.${T}.tok.bpe
+
---
+++ nematus/nematus/data_iterator.py
@@ -0,0 +1,181 @@
+import numpy
+
+import gzip
+
+import shuffle
+from util import load_dict
+
+def fopen(filename, mode='r'):
+    if filename.endswith('.gz'):
+        return gzip.open(filename, mode)
+    return open(filename, mode)
+
+class TextIterator:
+    """Simple Bitext iterator."""
+    def __init__(self, source, target,
+                 source_dicts, target_dict,
+                 batch_size=128,
+                 maxlen=100,
+                 n_words_source=-1,
+                 n_words_target=-1,
+                 skip_empty=False,
+                 shuffle_each_epoch=False,
+                 sort_by_length=True,
+                 use_factor=False,
+                 maxibatch_size=20,
+                 token_batch_size=0):
+        if shuffle_each_epoch:
+            self.source_orig = source
+            self.target_orig = target
+            self.source, self.target = shuffle.main([self.source_orig, self.target_orig], temporary=True)
+        else:
+            self.source = fopen(source, 'r')
+            self.target = fopen(target, 'r')
+        self.source_dicts = []
+        for source_dict in source_dicts:
+            self.source_dicts.append(load_dict(source_dict))
+        self.target_dict = load_dict(target_dict)
+
+        self.batch_size = batch_size
+        self.maxlen = maxlen
+        self.skip_empty = skip_empty
+        self.use_factor = use_factor
+
+        self.n_words_source = n_words_source
+        self.n_words_target = n_words_target
+
+        if self.n_words_source > 0:
+            for d in self.source_dicts:
+                for key, idx in d.items():
+                    if idx >= self.n_words_source:
+                        del d[key]
+
+        if self.n_words_target > 0:
+                for key, idx in self.target_dict.items():
+                    if idx >= self.n_words_target:
+                        del self.target_dict[key]
+
+        self.shuffle = shuffle_each_epoch
+        self.sort_by_length = sort_by_length
+
+        self.source_buffer = []
+        self.target_buffer = []
+        self.k = batch_size * maxibatch_size
+        self.token_batch_size = token_batch_size
+
+        self.end_of_data = False
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return sum([1 for _ in self])
+    
+    def reset(self):
+        if self.shuffle:
+            self.source, self.target = shuffle.main([self.source_orig, self.target_orig], temporary=True)
+        else:
+            self.source.seek(0)
+            self.target.seek(0)
+
+    def next(self):
+        if self.end_of_data:
+            self.end_of_data = False
+            self.reset()
+            raise StopIteration
+
+        source = []
+        target = []
+
+        longest_source = 0
+        longest_target = 0
+
+        # fill buffer, if it's empty
+        assert len(self.source_buffer) == len(self.target_buffer), 'Buffer size mismatch!'
+
+        if len(self.source_buffer) == 0:
+            for ss in self.source:
+                ss = ss.split()
+                tt = self.target.readline().split()
+                
+                if self.skip_empty and (len(ss) == 0 or len(tt) == 0):
+                    continue
+                if len(ss) > self.maxlen or len(tt) > self.maxlen:
+                    continue
+
+                self.source_buffer.append(ss)
+                self.target_buffer.append(tt)
+                if len(self.source_buffer) == self.k:
+                    break
+
+            if len(self.source_buffer) == 0 or len(self.target_buffer) == 0:
+                self.end_of_data = False
+                self.reset()
+                raise StopIteration
+
+            # sort by source/target buffer length
+            if self.sort_by_length:
+                tlen = numpy.array([max(len(s),len(t)) for (s,t) in zip(self.source_buffer,self.target_buffer)])
+                tidx = tlen.argsort()
+
+                _sbuf = [self.source_buffer[i] for i in tidx]
+                _tbuf = [self.target_buffer[i] for i in tidx]
+
+                self.source_buffer = _sbuf
+                self.target_buffer = _tbuf
+
+            else:
+                self.source_buffer.reverse()
+                self.target_buffer.reverse()
+
+
+        try:
+            # actual work here
+            while True:
+
+                # read from source file and map to word index
+                try:
+                    ss = self.source_buffer.pop()
+                except IndexError:
+                    break
+                tmp = []
+                for w in ss:
+                    if self.use_factor:
+                        w = [self.source_dicts[i][f] if f in self.source_dicts[i] else 1 for (i,f) in enumerate(w.split('|'))]
+                    else:
+                        w = [self.source_dicts[0][w] if w in self.source_dicts[0] else 1]
+                    tmp.append(w)
+                ss_indices = tmp
+
+                # read from source file and map to word index
+                tt = self.target_buffer.pop()
+                tt_indices = [self.target_dict[w] if w in self.target_dict else 1
+                      for w in tt]
+                if self.n_words_target > 0:
+                    tt_indices = [w if w < self.n_words_target else 1 for w in tt_indices]
+
+                source.append(ss_indices)
+                target.append(tt_indices)
+                longest_source = max(longest_source, len(ss_indices))
+                longest_target = max(longest_target, len(tt_indices))
+
+                if self.token_batch_size:
+                    if len(source)*longest_source > self.token_batch_size or \
+                        len(target)*longest_target > self.token_batch_size:
+                        # remove last sentence pair (that made batch over-long)
+                        source.pop()
+                        target.pop()
+                        self.source_buffer.append(ss)
+                        self.target_buffer.append(tt)
+
+                        break
+
+                else:
+                    if len(source) >= self.batch_size or \
+                        len(target) >= self.batch_size:
+                        break
+
+        except IOError:
+            self.end_of_data = True
+
+        return source, target
