5102a7142d0add45f3ab0a160ea5df4e989b03aa
Jacob Striebel <striebel@users.noreply.github.com>
2020-08-21 12:15:12 -0400

    Update evaluate.sh

--- wmt16-scripts/factored_sample/evaluate.sh
+++ wmt16-scripts/factored_sample/evaluate.sh
@@ -7,11 +7,11 @@ device=0
 this_dir=`dirname $0`
 nematus=$this_dir/../../nematus
 model="${this_dir}/model/${1}/model_${1}.best_valid_script"
-test="${this_dir}/data/${2}.factors_${1}.de"
-candidate_bpe="${this_dir}/eval/${1}/${2}.bpe.en.candidate"
-candidate_mrg="${this_dir}/eval/${1}/${2}.en.candidate"
-ref_bpe="${this_dir}/data/${2}.bpe.en"
-ref_mrg="${this_dir}/data/${2}.en"
+test="${this_dir}/data/newstest${2}.factors_${1}.de"
+candidate_bpe="${this_dir}/eval/${1}/newstest${2}.bpe.en.candidate"
+candidate_mrg="${this_dir}/eval/${1}/newstest${2}.en.candidate"
+ref_bpe="${this_dir}/data/newstest${2}.bpe.en"
+ref_mrg="${this_dir}/data/newstest${2}.en"
 
 mkdir $this_dir/eval
 mkdir $this_dir/eval/$1
