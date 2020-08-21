c021b13490a957ae1fe2dc7e8fd8a080b80758c2
Jacob Striebel <striebel@users.noreply.github.com>
2020-08-21 09:37:52 -0400

    Deleted the superseded train.sh

--- wmt16-scripts/factored_sample/train.sh
+++
@@ -1,5 +0,0 @@
-# theano device, in case you do not want to compute on gpu, change it to cpu
-#device=gpu
-device=cuda
-
-THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=$device,on_unused_input=warn python config_${1}.py
