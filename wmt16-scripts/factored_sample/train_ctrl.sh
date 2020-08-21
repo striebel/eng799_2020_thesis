c264ec8f425367edd69f10f4e4ac19841eea37b8
Jacob Striebel <striebel@users.noreply.github.com>
2020-08-21 09:45:41 -0400

    Refactored train_ctrl.sh

--- wmt16-scripts/factored_sample/train_ctrl.sh
+++ wmt16-scripts/factored_sample/train_ctrl.sh
@@ -4,12 +4,12 @@
 this_dir=`dirname $0`
 
 # Path to nematus
-nematus=../../nematus
+nematus=$this_dir/../../nematus
 
 # CUDA device id
 device=0
   
-CUDA_VISIBLE_DEVICES=$device python $nematus/nematus/train.py                               \
+CUDA_VISIBLE_DEVICES=$device python $nematus/nematus/train.py  \
   --model 'model/ctrl/model_ctrl.npz'                          \
   --reload latest_checkpoint                                   \
   --dim_word 500                                               \
@@ -27,15 +27,15 @@ CUDA_VISIBLE_DEVICES=$device python $nematus/nematus/train.py                   
   --target_dataset 'data/corpus.bpe.en'                        \
   --valid_source_dataset 'data/newstest2013.factors_ctrl.de'   \
   --valid_target_dataset 'data/newstest2013.bpe.en'            \
-  --dictionaries 'data/corpus.bpe.de.json'    \
-    'data/corpus.factors.1.de.json'           \
-    'data/corpus.bpe.en.json'                 \
-  --valid_freq 10000                          \
-  --disp_freq 1000                            \
-  --save_freq 30000                           \
-  --sample_freq 10000                         \
-  --rnn_dropout_embedding 0.2                 \
-  --rnn_dropout_hidden 0.2                    \
-  --rnn_dropout_source 0.1                    \
-  --rnn_dropout_target 0.1                    \
+  --dictionaries 'data/corpus.bpe.de.json'                     \
+    'data/corpus.factors.1.de.json'                            \
+    'data/corpus.bpe.en.json'                                  \
+  --valid_freq 10000                                           \
+  --disp_freq 1000                                             \
+  --save_freq 30000                                            \
+  --sample_freq 10000                                          \
+  --rnn_dropout_embedding 0.2                                  \
+  --rnn_dropout_hidden 0.2                                     \
+  --rnn_dropout_source 0.1                                     \
+  --rnn_dropout_target 0.1                                     \
   --valid_script "$this_dir/validate.sh"
